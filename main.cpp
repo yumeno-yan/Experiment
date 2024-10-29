36f7e4f7eb548d43df75b5ac4fca563bc424c85d
yumeno-yan <943549103@qq.com>
2024-10-29 16:42:55 +0800

    添加测试数据

--- main.cpp
+++ main.cpp
@@ -6,18 +6,15 @@ int main()
 {
 	// coh.init("/home/jieyan/Experiment");
 	// coh << "123\n" << "456";
-	//vector<Student> arr{ 1.5,2.5,3.5,4.5,6.5,7.5,7.5,1.5,5.5,6.5,9.5,11.5 };
-	//auto a = top_n(arr, 8);
-	//for (auto& s : a)
-	//{
-	//	cout << s.score << " ";
-	//}
-	//cout << "\n";
 	pf.significant_digits = 7;
-	auto ans = BigDecimal::calc_experssion("888484/114514");
+	// auto ans = BigDecimal::calc_experssion("888484/114514");
+	string str1, str2;
+	Ci_handler::read_number("../test_in1.txt", str1);
+	Ci_handler::read_number("../test_in2.txt", str2);
 	// BigDecimal a("888484");
 	// BigDecimal b("114514");
-	// auto ans = a / b;
+	BigDecimal a(str1), b(str2);
+	auto ans = a * b;
 	ans.show();
 	// pf.significant_digits = 5;
 	// string str = "1.123456";
